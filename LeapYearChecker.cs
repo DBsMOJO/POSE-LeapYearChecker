802dd8b7c05225f8de691d96aa8b5a05b38df929
DBsMOJO <d.binder1@students.htl-leonding.ac.at>
2023-11-12 11:28:25 +0100

    added code

---
+++ LeapYearChecker.cs
@@ -0,0 +1,20 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 3.ACIF
+ *--------------------------------------------------------------
+ *              Daniel Binder, 12.11.2023
+ *--------------------------------------------------------------
+ * Description:
+ * Checks the given year to see if it was a leap year
+ *--------------------------------------------------------------
+*/
+
+namespace LeapYearChecker
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int givenYear;
+        }
+    }
+}
